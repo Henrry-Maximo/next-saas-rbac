571ffae3ab64cb20cf2eff8780573d32321d4a46
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-07-16 17:02:47 -0300

    feat: config casl for roles

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,2 +1,19 @@
-const teste = 'oi'
-console.log(teste)
+import {
+  CreateAbility,
+  createMongoAbility,
+  ForcedSubject,
+  MongoAbility,
+} from '@casl/ability'
+
+const actions = ['manage', 'invite'] as const
+const subjects = ['User', 'all'] as const
+type AppAbilities = [
+  (typeof actions)[number],
+  (
+    | (typeof subjects)[number]
+    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
+  ),
+]
+
+export type AppAbility = MongoAbility<AppAbilities>
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
