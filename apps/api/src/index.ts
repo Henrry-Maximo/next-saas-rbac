61b45553bde6b85663ebfa0dc7aa728ac11dfff5
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-07-18 13:14:29 -0300

    feat: using perm in api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+import { ability } from '@saas/auth';
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+console.log(userCanInviteSomeoneElse)
+console.log(userCanDeleteOtherUsers)
+console.log(userCannotDeleteOtherUsers)
