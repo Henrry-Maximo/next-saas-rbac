257c7981930c2fe74cc306d13fb88535ef5bb418
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-08-01 16:55:54 -0300

    📦refactor: improving permissions and type with zod

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -16,7 +16,7 @@ export const permissions: Record<Role, PermissionsByRole> = {
     can('manage', 'all')
   },
   MEMBER: (_, { can }) => {
-    can('invite', 'User')
+    // can('invite', 'User')
     can('create', 'Project')
   },
 }
--- packages/auth/src/subjects/project.ts
+++ packages/auth/src/subjects/project.ts
@@ -1,1 +1,15 @@
-export type ProjectSubject = ['create' | 'delete' | 'manage', 'Project']
+import { z } from 'zod'
+
+export const projectSubject = z.tuple([
+  z.union([
+    z.literal('manage'),
+    z.literal('get'),
+    z.literal('create'),
+    z.literal('update'),
+    z.literal('delete'),
+  ]),
+  z.literal('Project'),
+])
+
+export type ProjectSubject = z.infer<typeof projectSubject>
+// export type ProjectSubject = ['create' | 'delete' | 'manage', 'Project']
